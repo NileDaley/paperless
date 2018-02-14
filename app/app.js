da6abd4f05451be4e0ffe6fee2d5abe1396a4a07
Unknown <joshhaz2012@gmail.com>
2018-02-14 14:49:47 +0000

    wrong file last time...

--- app/app.js
+++ app/app.js
@@ -24,6 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/api', api);
+app.get('/counter', function(req, res) {
+  res.sendFile(__dirname + '/modules/counter/counter.html');
+});
+
+app.get('/admin', function(req, res) {
+  res.sendFile(__dirname + '/modules/admin/admin.html');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
