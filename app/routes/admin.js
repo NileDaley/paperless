f17b2773e993b8668b0c70ff54cdb7bc79e64d2d
Unknown <joshhaz2012@gmail.com>
2018-02-14 14:48:34 +0000

    routes to /admin html

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('you\'re at the admin api');
+  res.send('../modules/admin/admin.html');
 });
 
 module.exports = router;
