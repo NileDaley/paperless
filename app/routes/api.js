e0a7b77f553b12b98c2c9ebeb0885f35c14807d6
Nile Daley <NileDaley@outlook.com>
2018-02-19 20:43:18 +0000

    Added table buttons

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const counter = require('./counter.js');
 const kitchen = require('./kitchen.js');
 const admin = require('./admin.js');
+const waiter = require('./waiter.js');
 
 router.use('/counter', counter);
 router.use('/kitchen', kitchen);
 router.use('/admin', admin);
+router.use('/waiter', waiter);
 
 module.exports = router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  
+  /*
+    TODO: Get food items from database
+    TODO: res.render('index', { foodItems: <foodItems> });
+  */
+  res.render('index', {
+    tables: [1,2,3,4,5,6,7,8,9,10]
+  });
 });
 
 module.exports = router;
