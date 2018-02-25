aea031da82ddee30b1ba9c78d3102be44b4c257b
Nile Daley <NileDaley@outlook.com>
2018-02-25 10:18:48 +0000

    Removed unnecessary /waiter api endpoint

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const counter = require('./counter.js');
 const kitchen = require('./kitchen.js');
 const admin = require('./admin.js');
-const waiter = require('./waiter.js');
 const menu = require('./menu.js');
 
 router.use('/counter', counter);
 router.use('/kitchen', kitchen);
 router.use('/admin', admin);
-router.use('/waiter', waiter);
 router.use('/menu', menu);
 
 module.exports = router;
