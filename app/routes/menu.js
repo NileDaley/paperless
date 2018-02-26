8945d5f0549f35abd7a3d5e6c3485d349f4fffc5
Nile Daley <NileDaley@outlook.com>
2018-02-26 10:51:32 +0000

    Moved db connection to api.js to share connection amongst routes

--- app/routes/menu.js
+++ app/routes/menu.js
@@ -3,13 +3,6 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const FoodItemSchema = require('../schemas/FoodItemSchema');
 
-mongoose.connect('mongodb://admin:password@ds247078.mlab.com:47078/paperless');
-mongoose.Promise = global.Promise;
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log("Connected to mongodb via mongoose..."));
-
 router.get('/', function (req, res, next) {
   let FoodItem = mongoose.model('FoodItem', FoodItemSchema);
   FoodItem
