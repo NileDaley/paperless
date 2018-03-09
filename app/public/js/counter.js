c0d1c99ce782bf08e2861143c8cb67ffb1856bc5
Unknown <joshhaz2012@gmail.com>
2018-03-09 19:57:34 +0000

    Updated to use real data.
    Displays real table data, or a relevant message if the table is empty.

--- app/public/js/counter.js
+++ app/public/js/counter.js
@@ -2,10 +2,15 @@ var SelectTable = {
   template: '<div><h2>Select a table to see the order</h2></div>'
 };
 
+var EmptyTable = {
+  template: '<div><h2>Table is empty!</h2></div>'
+};
+
 var app = new Vue({
   el: '#app',
   data: {
     selected: false,
+    empty: false,
     selectedTable: null,
     socket: null,
     currentBill: {
@@ -14,59 +19,71 @@ var app = new Vue({
       totalPreTax: 0,
       vatAmount: 0,
       totalPostTax: 0,
-      order: {
-        item: 0,
-        quantity: 0,
-        price: 0
-      }
+      order: []
     },
     existingOrders: [],
     testOrders: [],
     tables: []
   },
   components: {
-    'select-table': SelectTable
+    'select-table': SelectTable,
+    'empty-table': EmptyTable
   },
   methods: {
 
     showTableOrder(tableNo) {
-      this.currentBill.tableNo = tableNo;
-      this.currentBill.orderNo = this.tables[tableNo - 1].orderNo;
-      this.currentBill.order = this.tables[tableNo - 1].order;
-      this.selected = true;
+      this.empty = false
+      let currentTable = [];
+      currentTable = this.existingOrders.filter(order => {
+        return order['table'] === tableNo;
+      })
+
+      if (currentTable.length === 0) {
+        this.selected = true;
+        this.empty = true;
+      } else {
+        this.currentBill.tableNo = tableNo;
+        this.currentBill.orderNo = currentTable[0]._id;
+        const extractedItems = this.extractItemsFromCourses(currentTable[0].order);
+        this.currentBill.order = extractedItems;
+        this.calculatePreTax();
+        this.calculateVAT();
+        this.calculateTotal();
+        this.selected = true;
+        console.log(this.currentBill.totalPreTax);
+        console.log(this.currentBill.vatAmount);
+        console.log(this.currentBill.totalPostTax);
+      }
     },
 
-    totalPreTax() {
+    calculatePreTax() {
       let total = 0;
       /* TODO: Update forEach statement to reflect changes of order structure. See /api/all-orders for new structure */
-      this.tables[this.currentBill.tableNo - 1].order.forEach(element => {
+      this.currentBill.order.forEach(element => {
         const itemTotal = element.quantity * element.price;
         total += itemTotal;
       });
       this.currentBill.totalPreTax = total;
-      return total;
     },
 
     calculateVAT() {
       const vat = this.currentBill.totalPreTax * 0.2;
       this.currentBill.vatAmount = vat;
-      return vat;
     },
 
     calculateTotal() {
       const total = this.currentBill.totalPreTax + this.currentBill.vatAmount;
       this.currentBill.totalPostTax = total;
-      return total;
     },
 
     completeOrder() {
       axios.post('/api/counter/complete-order', this.currentBill)
-          .then(function(response) {
-            console.log(response);
-          })
-          .catch(function(error) {
-            console.log(error);
-          });
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
       alert('Saving data for table no: ' + JSON.stringify(this.currentBill));
       console.log(this.currentBill);
     },
@@ -74,22 +91,11 @@ var app = new Vue({
     // finish this
     getOrders() {
       axios.get('/api/counter/all-orders')
-          .then(response => {
-            let allOrders = response['data'];
-            let existingOrders = allOrders.filter(o => o.status !== 'paid' && o.status !== 'abandoned');
-
-            let testOrders = []
-
-            existingOrders.forEach(order => {
-              items = this.extractItemsFromCourses(order['order']);
-              let tableNo = order['table'];
-              testOrders.push(order);
-            });
-            // Vue.set()
-            return testOrders;
-            // console.log()
-          })
-          .catch(err => console.log(err));
+        .then(response => {
+          let allOrders = response['data'];
+          this.existingOrders = allOrders.filter(o => o.status !== 'paid' && o.status !== 'abandoned');
+        })
+        .catch(err => console.log(err));
     },
 
     extractItemsFromCourses(order) {
@@ -97,20 +103,43 @@ var app = new Vue({
       // Extract all items from each of the courses
       let items = [];
       for (let course of ['starters', 'mains', 'sides', 'desserts']) {
-        // console.log(order[course]);
-        items.push(order[course].items);
+        if (order[course].items.length !== 0) {
+          order[course].items.forEach(x => {
+            let name = x['item'].name;
+            let quantity = x.quantity;
+            let price = x['item'].price;
+            items.push(new Item(name, quantity, price));
+
+          });
+        }
       }
-      // console.log(items);
       return items;
+    },
+
+    selectedAndNotEmpty() {
+      let bool = false;
+
+      if (!this.empty && this.selected) {
+        bool = true;
+      }
+
+      return bool;
     }
   },
-  created: function() {
-    // this.testOrders;
-    this.testOrders = this.getOrders();
-    console.log(this.testOrders);
+
+  created: function () {
+    this.getOrders();
     this.socket = io.connect();
     this.socket.on('newOrder', newOrder => {
       // Do something here with new orders
     });
   }
 });
+
+class Item {
+  constructor(name, quantity, price) {
+    this.name = name;
+    this.quantity = quantity;
+    this.price = price;
+  }
+}
