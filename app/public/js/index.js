46b0247803d059d7e55ddcfddc04d452f77115a0
Nile Daley <NileDaley@outlook.com>
2018-03-12 21:03:57 +0000

    Orders now update correctly, each course can be marked as served

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -7,7 +7,12 @@ class Table {
 }
 
 class Ticket {
-  constructor(_id = null, status = 'pending', date = null, order = new Order()) {
+  constructor(
+    _id = null,
+    status = 'pending',
+    date = null,
+    order = new Order()
+  ) {
     this._id = _id;
     this.status = status;
     this.date = date;
@@ -16,7 +21,13 @@ class Ticket {
 }
 
 class Order {
-  constructor(_id = null, starters = new Course(), mains = new Course(), sides = new Course(), desserts = new Course()) {
+  constructor(
+    _id = null,
+    starters = new Course(),
+    mains = new Course(),
+    sides = new Course(),
+    desserts = new Course()
+  ) {
     this._id = _id;
     this.starters = starters;
     this.mains = mains;
@@ -62,31 +73,33 @@ var app = new Vue({
     socket: null
   },
   computed: {
-    ticket: function () {
+    ticket: function() {
       return this.currentTable.ticket;
     },
-    order: function () {
+    order: function() {
       return this.ticket.order;
     },
-    starters: function () {
+    starters: function() {
       return this.order.starters;
     },
-    mains: function () {
+    mains: function() {
       return this.order.mains;
     },
-    sides: function () {
+    sides: function() {
       return this.order.sides;
     },
-    desserts: function () {
+    desserts: function() {
       return this.order.desserts;
     },
-    emptyOrder: function () {
-      return this.starters.items.length === 0
-        && this.mains.items.length === 0
-        && this.sides.items.length === 0
-        && this.desserts.items.length === 0
+    emptyOrder: function() {
+      return (
+        this.starters.items.length === 0 &&
+        this.mains.items.length === 0 &&
+        this.sides.items.length === 0 &&
+        this.desserts.items.length === 0
+      );
     },
-    pendingOrder: function () {
+    pendingOrder: function() {
       return this.currentTable.ticket.status === 'pending';
     }
   },
@@ -98,37 +111,51 @@ var app = new Vue({
       }
     },
     getFoodItems() {
-      axios.get('/api/menu').then(response => {
-        let data = response.data;
-        // Convert the json response to FoodItem objects and sort alphabetically
-        this.filteredFoodItems = this.foodItems = data.map(
-          item => new FoodItem(item)
-        ).sort((item, other) => {
-          return item.name.toLowerCase().localeCompare(other.name.toLowerCase());
-        });
-      }).catch(err => console.log(err));
+      axios
+        .get('/api/menu')
+        .then(response => {
+          let data = response.data;
+          // Convert the json response to FoodItem objects and sort alphabetically
+          this.filteredFoodItems = this.foodItems = data
+            .map(item => new FoodItem(item))
+            .sort((item, other) => {
+              return item.name
+                .toLowerCase()
+                .localeCompare(other.name.toLowerCase());
+            });
+        })
+        .catch(err => console.log(err));
     },
     getExistingOrders() {
-      axios.get('/api/counter/all-orders')
+      axios
+        .get('/api/counter/all-orders')
         .then(response => {
-
           let allOrders = response['data'];
-          let existingOrders = allOrders.filter(o => o.status !== 'paid' && o.status !== 'abandoned');
+          let existingOrders = allOrders.filter(
+            o => o.status !== 'paid' && o.status !== 'abandoned'
+          );
 
           existingOrders.forEach(order => {
-
             let orderTable = this.tables.find(t => t.number === order.table);
             orderTable.occupied = true;
             orderTable.customers = order.customers;
             // orderTable.order.status = order.status;
             // orderTable.order._id = order._id;
             // orderTable.order.items = this.extractItemsFromCourses(order);
-            let ticket = new Ticket(order._id, order.status, order.date, order.order);
+            let ticket = new Ticket(
+              order._id,
+              order.status,
+              order.date,
+              order.order
+            );
 
-            orderTable.ticket = new Ticket(order._id, order.status, order.date, order.order);
-
+            orderTable.ticket = new Ticket(
+              order._id,
+              order.status,
+              order.date,
+              order.order
+            );
           });
-
         })
         .catch(err => console.log(err));
     },
@@ -156,7 +183,6 @@ var app = new Vue({
       this.filterFoodItems(this.foodCategory, this.searchCriteria);
     },
     filterFoodItems(category, criteria) {
-
       this.filteredFoodItems = this.foodItems;
 
       if (category !== 'all') {
@@ -170,41 +196,44 @@ var app = new Vue({
           return item.name.toLowerCase().includes(criteria.toLowerCase());
         });
       }
-
     },
     addItemToOrder(foodItem) {
-
       if (this.pendingOrder) {
         const course = this[`${foodItem.category}s`];
+        course.status = 'pending';
         if (course.items.length > 0) {
-
-          const courseItem = course.items.find(orderItem => orderItem.item._id === foodItem._id);
+          const courseItem = course.items.find(
+            orderItem => orderItem.item._id === foodItem._id
+          );
           if (courseItem === undefined) {
-            course.items.push(new OrderItem({
+            course.items.push(
+              new OrderItem({
+                _id: null,
+                item: foodItem,
+                quantity: 1,
+                status: 'pending'
+              })
+            );
+          } else {
+            courseItem.quantity++;
+          }
+        } else {
+          course.items.push(
+            new OrderItem({
               _id: null,
               item: foodItem,
               quantity: 1,
               status: 'pending'
-            }));
-          } else {
-            courseItem.quantity++;
-          }
-
-        } else {
-          course.items.push(new OrderItem({
-            _id: null,
-            item: foodItem,
-            quantity: 1,
-            status: 'pending'
-          }));
+            })
+          );
         }
-
       }
-
     },
     removeItemFromOrder(foodItem) {
       const course = this[`${foodItem.category}s`];
-      const courseItem = course.items.find(orderItem => orderItem.item === foodItem);
+      const courseItem = course.items.find(
+        orderItem => orderItem.item === foodItem
+      );
       courseItem.quantity--;
       if (courseItem.quantity <= 0) {
         course.items.splice(course.items.indexOf(courseItem), 1);
@@ -212,14 +241,13 @@ var app = new Vue({
     },
     submitOrder() {
       if (this.currentTable.customers > 0 && this.pendingOrder) {
-
         const payload = this.getOrderPayload();
 
         if (this.ticket._id === null) {
-          axios.post('/', payload)
+          axios
+            .post('/', payload)
             .then(response => {
               if (response.status === 201) {
-
                 let submittedOrder = response['data'];
 
                 // Update the table's order
@@ -228,7 +256,6 @@ var app = new Vue({
 
                 // Tell the other parts of the system about the new order
                 this.socket.emit('newOrder', submittedOrder);
-
               }
             })
             .catch(err => console.log(err));
@@ -239,29 +266,36 @@ var app = new Vue({
       }
     },
     updateOrder(payload) {
-      axios.patch(`/${this.ticket._id}`, payload)
+      axios
+        .patch(`/${this.ticket._id}`, payload)
         .then(response => {
-
+          console.log(response);
           let updatedOrder = response['data'];
           this.ticket.status = updatedOrder.status;
           this.socket.emit('orderStateChange', updatedOrder);
-
         })
         .catch(err => console.log(err));
     },
     getOrderPayload() {
-
       return {
         table: this.currentTable.number,
         customers: this.currentTable.customers,
         order: this.currentTable.ticket.order,
         date: this.currentTable.ticket.date
-      }
-  
+      };
     },
-    serveOrder() {
+    serveCourse(course) {
+      this[course].status = 'served';
+      this[course].items.forEach(i => {
+        i.status = 'served';
+      });
+
+      this.ticket.status = this.allCoursesServed(this.ticket.order)
+        ? 'served'
+        : this.ticket.status;
+
       let payload = this.getOrderPayload();
-      payload.status = 'served';
+      payload.status = this.ticket.status;
       this.updateOrder(payload);
     },
     editOrder() {
@@ -278,14 +312,13 @@ var app = new Vue({
         if (table.ticket.order[course].status === 'ready') {
           anyReady = true;
         }
-      })
+      });
       return anyReady;
     },
     getTableClass(table) {
       if (this.anyCourseReady(table)) {
         return 'is-danger';
       } else {
-
         let className;
         if (table.customers > 0) {
           className = 'is-info';
@@ -296,14 +329,25 @@ var app = new Vue({
             className = 'is-warning';
             break;
           case 'served':
-            className = 'is-success'  
+            className = 'is-success';
         }
 
         return className;
       }
+    },
+    allCoursesServed(order) {
+      ['starters', 'mains', 'sides', 'desserts'].forEach(course => {
+        if (
+          order[course].items.length > 0 &&
+          order[course].status !== 'served'
+        ) {
+          return false;
+        }
+      });
+      return true;
     }
   },
-  created: function () {
+  created: function() {
     this.createTables();
     this.getFoodItems();
     this.getExistingOrders();
@@ -313,18 +357,13 @@ var app = new Vue({
       const table = this.tables.find(t => t.number === order.table);
 
       if (order.status === 'paid' || order.status === 'abandoned') {
-
         // Reset the table
         table.customers = 0;
         table.ticket = new Ticket();
         table.occupied = false;
-
       } else {
-
         table.ticket = order;
-
       }
-
     });
   }
 });
